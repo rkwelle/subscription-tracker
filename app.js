8c4ab06aa2f495e19d04815bd261f45c07227791
Ransom Kwelle <ransom_gentle@yahoo.com>
2025-06-16 18:10:19 -0700

    feat: add routes for project

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 import express from 'express';
 
+import { PORT } from './config/env.js';
+import authRouter from './routes/auth.route.js';
+import userRouter from './routes/user.route.js';
+import subscriptionRouter from './routes/subscription.route.js';
+
+
+
 const app = express();
 
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World! \n Thanks Ransom.');
 });
 
-app.listen(3000, () => {
-  console.log('Subscription Tracker API is running on http://localhost:3000');
+app.listen(PORT, () => {
+  console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
 });
 
 export default app;
